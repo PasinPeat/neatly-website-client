8d6d9182a223035c2ea0226ed03a79d1629d5b2b
PasinPeat <pasin.lawn1995@gmail.com>
2023-08-31 16:16:46 +0700

    Feat: add color variable & set font

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-
-import './App.css'
+import "./App.css";
 
 function App() {
-  
-
   return (
     <>
-      <div>
-        <h1 className=' text-center text-lg bg-green-700'>hello world</h1>
-        <button className="btn btn-primary ">Primary</button>
+      <div data-theme="mytheme">
+        <h1 className="w-1/2 h-32 text-center text-headline2 font-noto-serif-display bg-gray-800">Neatly Hotel</h1>
+        <h1 className="w-1/2 h-32 text-center text-headline2 font-noto-serif-display bg-clean">hello world</h1>
+        <h1 className="w-1/2 h-32 text-center text-headline2 font-noto-serif-display bg-dirty">hello world</h1>
+        <h1 className="w-1/2 h-32 text-center text-headline2 font-inter bg-inspected">hello world</h1>
+        <button className="btn btn-base-200">Primary</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,118 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
-}
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      green: {
+        100: "#F1F5F3",
+        200: "#E6EBE9",
+        300: "#D5DFDA",
+        400: "#ABC0B4",
+        500: "#81A08F",
+        600: "#5D7B6A",
+        700: "#465C50",
+        800: "#2F3E35",
+        900: "#171F1B",
+      },
+      orange: {
+        100: "#FAEDE8",
+        200: "#F9DACE",
+        300: "#F3B59C",
+        400: "#ED906B",
+        500: "#E76B39",
+        600: "#C14817",
+        700: "#803010",
+        800: "#631F04",
+        900: "#401808",
+      },
+      gray: {
+        100: "#F6F7FC",
+        200: "#F1F2F6",
+        300: "#E4E6ED",
+        400: "#D6D9E4",
+        500: "#C8CCDB",
+        600: "#9AA1B9",
+        700: "#646D89",
+        800: "#424C6B",
+        900: "#2A2E3F",
+      },
+      white: "#FFFFFF",
+      black: "#000000",
+      red: "#B61515",
+      bg: "#F7F7FB",
+      vacant: "#F0F2F8",
+      out: "#F0F1F8",
+      occupied: "#E4ECFF",
+      clean: "#E5FFFA",
+      dirty: "#FFE5E5",
+      inspected: "#FFF9E5",
+    },
 
+    fontSize: {
+      body3: [
+        "12px",
+        {
+          lineHeight: "18px",
+          fontWeight: "500",
+        },
+      ],
+      body2: [
+        "14px",
+        {
+          lineHeight: "21px",
+          fontWeight: "500",
+        },
+      ],
+      body1: [
+        "16px",
+        {
+          lineHeight: "24px",
+          fontWeight: "400",
+        },
+      ],
+      headline5: [
+        "20px",
+        {
+          lineHeight: "30px",
+          fontWeight: "600",
+        },
+      ],
+      headline4: [
+        "28px",
+        {
+          lineHeight: "42px",
+          fontWeight: "600",
+        },
+      ],
+      headline3: [
+        "44px",
+        {
+          lineHeight: "55px",
+          fontWeight: "500",
+        },
+      ],
+      headline2: [
+        "68px",
+        {
+          lineHeight: "85px",
+          fontWeight: "400",
+        },
+      ],
+      headline1: [
+        "88px",
+        {
+          lineHeight: "110px",
+          fontWeight: "500",
+        },
+      ],
+    },
+    fontFamily: {
+      inter: ["Inter"],
+      'noto-serif-display': ['Noto Serif Display'],
+    },
+  },
+
+  plugins: [require("daisyui")],
+};
