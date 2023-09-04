8fa6b925108a8a88564a30be0ec9096fb1ac2348
cbtab <chalermchat.tab@gmail.com>
2023-09-04 17:06:18 +0700

    feat: add form state

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,7 +1,42 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function Register() {
+  const navigate = useNavigate();
+  //user's profile
+  const [fullName, setFullName] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [idNumber, setIdNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthDay, setBirthDay] = useState("");
+  const [country, setCountry] = useState("");
+
+  //user's credit card
+  const [cardNumber, setCardNumber] = useState("");
+  const [cardOwner, setCardOwner] = useState("");
+  const [expireDate, setExpireDate] = useState("");
+  const [cardCode, setCardCode] = useState("");
+
+  //handle form
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const data: object = {
+      fullName,
+      username,
+      password,
+      idNumber,
+      email,
+      birthDay,
+      country,
+    };
+    await axios.post("http://localhost:4000/auth/register", data);
+    navigate("/login");
+  };
+
   return (
     <div className="flex flex-col items-center w-screen bg-coverRegister bg-cover">
       <Navbar />
@@ -11,7 +46,12 @@ function Register() {
           Register
         </h1>
         <p className="text-gray-600 text-headline5 py-10">Basic Information</p>
-        <form className="w-full">
+        <form
+          className="w-full"
+          onSubmit={(event) => {
+            handleSubmit(event);
+          }}
+        >
           <div>
             <label htmlFor="fname">
               <p className="font-body1 text-gray-900  text-start">Full Name</p>
@@ -19,7 +59,11 @@ function Register() {
             <input
               type="text"
               id="fname"
+              value={fullName}
               name="fname"
+              onChange={(e) => {
+                setFullName(e.target.value);
+              }}
               placeholder="Enter your name and lastname"
               className="w-full Input mb-10"
               required
@@ -33,8 +77,12 @@ function Register() {
               </label>
               <input
                 type="text"
+                value={username}
                 id="username"
                 name="username"
+                onChange={(e) => {
+                  setUsername(e.target.value);
+                }}
                 placeholder="Enter your username"
                 className="w-full Input"
                 required
@@ -48,7 +96,11 @@ function Register() {
               <input
                 type="email"
                 id="email"
+                value={email}
                 name="email"
+                onChange={(e) => {
+                  setEmail(e.target.value);
+                }}
                 placeholder="Enter your Email"
                 className="w-full Input"
                 required
@@ -62,7 +114,11 @@ function Register() {
               <input
                 type="password"
                 id="password"
+                value={password}
                 name="password"
+                onChange={(e) => {
+                  setPassword(e.target.value);
+                }}
                 placeholder="Enter your Password"
                 className="w-full Input"
                 required
@@ -78,7 +134,11 @@ function Register() {
               <input
                 type="tel"
                 id="idNumber"
+                value={idNumber}
                 name="idNumber"
+                onChange={(e) => {
+                  setIdNumber(e.target.value);
+                }}
                 pattern="\d*"
                 maxLength={13}
                 placeholder="Enter your ID Number"
@@ -96,7 +156,11 @@ function Register() {
               <input
                 type="date"
                 id="birthDate"
+                value={birthDay}
                 name="birthDate"
+                onChange={(e) => {
+                  setBirthDay(e.target.value);
+                }}
                 placeholder="Select your date of birth"
                 className="w-full Input"
                 required
@@ -110,6 +174,10 @@ function Register() {
               <select
                 name="country"
                 id="country"
+                value={country}
+                onChange={(e) => {
+                  setCountry(e.target.value);
+                }}
                 placeholder="Select your country"
                 className="w-full Input"
               >
@@ -152,7 +220,11 @@ function Register() {
                 type="tel"
                 id="cardNumber"
                 name="cardNumber"
+                value={cardNumber}
                 pattern="\d*"
+                onChange={(e) => {
+                  setCardNumber(e.target.value);
+                }}
                 maxLength={10}
                 placeholder="Enter your card number"
                 className="w-full Input font-body1"
@@ -169,6 +241,10 @@ function Register() {
               <input
                 type="tel"
                 id="expried"
+                value={expireDate}
+                onChange={(e) => {
+                  setExpireDate(e.target.value);
+                }}
                 name="expried"
                 pattern="\d*"
                 maxLength={4}
@@ -187,6 +263,10 @@ function Register() {
               <input
                 type="text"
                 id="cardOwner"
+                value={cardOwner}
+                onChange={(e) => {
+                  setCardOwner(e.target.value);
+                }}
                 name="cardOwner"
                 placeholder="Enter your name"
                 className="w-full Input font-body1"
@@ -202,6 +282,10 @@ function Register() {
                 type="tel"
                 id="cvc"
                 name="cvc"
+                value={cardCode}
+                onChange={(e) => {
+                  setCardCode(e.target.value);
+                }}
                 pattern="\d*"
                 maxLength={3}
                 placeholder="CVC/CVV"
