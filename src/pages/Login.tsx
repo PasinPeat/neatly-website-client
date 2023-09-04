1c00103b42f2f8eeb64b8c4d4ad487e9d19ba321
Tongcnc <tongcnc3@gmail.com>
2023-09-04 10:54:19 +0700

    feat: Styling Login and Register Page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,76 @@
-import React from 'react'
+import React from "react";
+import Navbar from "../components/Navbar";
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <div className="flex flex-col justify-center items-center ">
+      <Navbar />
+      <div className="flex flex-row">
+        <div className="flex-1">
+          <img
+            src="https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/images/superior-garden-3?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pbWFnZXMvc3VwZXJpb3ItZ2FyZGVuLTMiLCJpYXQiOjE2OTM1NDk2NzksImV4cCI6MTcyNTA4NTY3OX0.s3rwwqDSUUgQ_o5RwmhBssmWOz0v3UR5w-ftMYaEJI4&t=2023-09-01T06%3A27%3A59.603Z"
+            alt="hotel-exterior"
+            className="object-cover w-full h-full"
+          />
+        </div>
+
+        <div className=" p-[120px] bg-bg shadow-md lg:max-w-xl flex flex-col justify-center flex-1">
+          <h1 className="text-[68px] font-noto-serif-display font-medium text-green-800 mb-[60px]">
+            Log In
+          </h1>
+          <form className="space-y-4">
+            <div>
+              <label className="label">
+                <span className="font-inter font-body1 text-gray-900">
+                  Username or Email
+                </span>
+              </label>
+              <input
+                type="text"
+                id="username"
+                name="username"
+                placeholder="Enter username or email"
+                className="w-full Input"
+              />
+            </div>
+
+            <div>
+              <label className="label">
+                <span className="font-inter font-body1 text-gray-900">
+                  Password
+                </span>
+              </label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                placeholder="Enter your Password"
+                className="w-full Input"
+              />
+            </div>
+
+            <div>
+              <button
+                className="btn Button w-full border-none mb-4"
+                type="submit"
+              >
+                Log In
+              </button>
+            </div>
+            <p className="text-gray-700 font-body1 text-start">
+              Don't have an account yet?{" "}
+              <a
+                href="/register"
+                className="text-orange-500 font-semibold hover:underline"
+              >
+                Register
+              </a>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
