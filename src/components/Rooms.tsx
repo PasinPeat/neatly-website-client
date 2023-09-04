8fcd33af5654c71a157e0221799d068deba3148f
unknown <ratiporn.pr@gmail.com>
2023-09-04 14:37:31 +0700

    Style: rooms section in Home page

--- src/components/Rooms.tsx
+++ src/components/Rooms.tsx
@@ -1,10 +1,16 @@
 function Rooms() {
   return (
-    <div className="py-28 flex flex-col items-center justify-center bg-white">
+    <div className="py-28 px-40 flex flex-col items-center justify-center bg-white">
       <div className="font-noto-serif-display text-green-800 text-headline3">
         Rooms & Suits
       </div>
-      <div></div>
+      <div className="mt-[72px] w-full bg-green-200 grid gap-6 grid-rows-4 grid-cols-5 ">
+        <div>1</div>
+        <div>2</div>
+        <div>3</div>
+        <div>4</div>
+        <div>5</div>
+      </div>
     </div>
   );
 }
