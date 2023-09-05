75ed05090c4744d0249d1be1090fab6cbafad080
unknown <ratiporn.pr@gmail.com>
2023-09-06 02:33:03 +0700

    fix: fetch data to render

--- src/App.tsx
+++ src/App.tsx
@@ -8,34 +8,38 @@ import SearchResult from "./pages/SearchResult.tsx";
 import axios from "axios";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { useParams, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { RoomsProps } from "./interfaces/RoomsProps.tsx";
-// export const RoomDetailContext = React.createContext();
+export const RoomsContext = React.createContext();
 
 function App() {
-  // const [rooms, setRooms] = useState<Room[]>([]);
+  const [rooms, setRooms] = useState<RoomsProps[]>([]);
   // const params = useParams();
   // const navigate = useNavigate();
 
-  // const getRooms = async () => {
-  //   const results = await axios(`http://localhost:4000/room/${params.roomId}`);
-  //   setRooms(results.data.data);
-  //   console.log(results);
-  // };
+  const getRooms = async () => {
+    const results = await axios(`http://localhost:4000/room`);
+    setRooms(results.data.data);
+    // console.log(results);
+  };
+
+  useEffect(() => {
+    getRooms();
+  }, []);
 
   return (
-    // <RoomDetailContext.Provider value={{ rooms }}>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/room-detail" element={<RoomDetail />} />
-        <Route path="/search" element={<SearchResult />} />
-        {/* <Route path="/*" element={<NotFound />} /> */}
-      </Routes>
-    </BrowserRouter>
-    // </RoomDetailContext.Provider>
+    <RoomsContext.Provider value={{ rooms }}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/room-detail" element={<RoomDetail />} />
+          <Route path="/search" element={<SearchResult />} />
+          {/* <Route path="/*" element={<NotFound />} /> */}
+        </Routes>
+      </BrowserRouter>
+    </RoomsContext.Provider>
   );
 }
 
