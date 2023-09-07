4e3886ac30a138f3dfe620ea0256070d6555bd48
unknown <ratiporn.pr@gmail.com>
2023-09-08 00:38:04 +0700

    Feat: add default search result

--- src/components/RoomResultCard.jsx
+++ src/components/RoomResultCard.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 
-function RoomResultCard() {
+function RoomResultCard({
+  roomId,
+  roomImages,
+  roomType,
+  bedType,
+  description,
+  area,
+  price,
+  promotionPrice,
+  amenity,
+  person,
+  available,
+}) {
+  // console.log(roomType);
+  // console.log(person);
+
+  const backgroundImage = {
+    backgroundImage: `url('${roomImages[2]}')`,
+  };
+
   return (
     <div>
       <div className="justify-between items-start gap-14 inline-flex bg-bg border-b-[1px] border-gray-300 text-gray-700">
         <div className=" flex gap-12 py-10">
           <div className="relative">
-            <div className="w-[453px] h-[320px] rounded  bg-cover bg-[url('https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/images/superior-graden-2?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pbWFnZXMvc3VwZXJpb3ItZ3JhZGVuLTIiLCJpYXQiOjE2OTM1NTYxMjMsImV4cCI6MTcyNTA5MjEyM30.MAuhRFQuQ_RgNd6NTNFDOR5Eygp-vv_7pfB1Biot2KQ&t=2023-09-01T08%3A15%3A23.657Z')]"></div>
+            <div
+              style={backgroundImage}
+              className="w-[453px] h-[320px] rounded bg-cover bg-center"
+            ></div>
             <button className="absolute bottom-0 left-0 rounded-tr-lg opacity-60 bg-white p-2">
               <div className="w-6 h-6 opacity-60 bg-[url('https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/icon/image.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pY29uL2ltYWdlLnN2ZyIsImlhdCI6MTY5Mzg4NjI4NSwiZXhwIjoxNzI1NDIyMjg1fQ.44CiKMoxMLmc_20EW3RzoUJyldns-KueYSKKLT6HLy8&t=2023-09-05T03%3A58%3A05.908Z')]"></div>
             </button>
@@ -14,38 +36,35 @@ function RoomResultCard() {
           <div className="flex flex-col py-6 justify-between">
             <div className="flex gap-6">
               <div className="w-[314px] text-left">
-                <h2 className="text-headline4 text-black">
-                  Superior Garden View
-                </h2>
-                <div className="pb-8">
-                  <span className="text-gray-800">2</span>
+                <h2 className="text-headline4 text-black">{roomType}</h2>
+                <div className="pb-8 text-gray-800">
+                  <span>{person}</span>
                   <span> Guests</span>
                   <span className="px-3"> | </span>
-                  <span className="text-gray-800">1 </span>
-                  <span> Double bed</span>
+                  <span>{bedType}</span>
                   <span className="px-3"> | </span>
-                  <span className="text-gray-800">32</span>
+                  <span>{area}</span>
                   <span> sqm</span>
                 </div>
-                <p className="text-body1">
-                  Rooms (36sqm) with full garden views, 1 single bed, bathroom
-                  with bathtub & shower.
-                </p>
+                <p className="text-body1">{description}</p>
               </div>
 
               {/* right elements */}
               <div className="flex flex-col text-right text-body1 w-64">
-                <p className="line-through pt-2">THB 3,100.00</p>
-                <p className="text-headline5 text-black pb-3">THB 2,500.00</p>
+                <p className="line-through pt-2">THB {price}</p>
+                <p className="text-headline5 text-black pb-3">
+                  THB {promotionPrice}
+                </p>
                 <p>
                   Per Night
                   <br />
                   (Including Taxes & Fees)
                 </p>
-                <p className="pt-2">
-                  Avialable
-                  <span>___</span>
-                  <span>rooms</span>
+                <p className="pt-2 text-black font-semibold">
+                  <span className="pr-1">{available}</span>
+                  {available === 1 && <span>room</span>}
+                  {available > 1 && <span>rooms</span>}
+                  <span className="pl-1">available</span>
                 </p>
               </div>
             </div>
--- src/pages/SearchResult.tsx
+++ src/pages/SearchResult.tsx
@@ -3,8 +3,11 @@ import Navbar from "../components/Navbar";
 import RoomResultCard from "../components/RoomResultCard";
 import Search from "../components/Search";
 import Footer from "../components/Footer";
+import { useContext } from "react";
+import { RoomsContext } from "../App.jsx";
 
 function SearchResult() {
+  const context = useContext(RoomsContext);
   const buttonStyle = {
     borderStyle: "solid",
     borderColor: "#E76B39",
@@ -12,6 +15,9 @@ function SearchResult() {
     backgroundColor: "white",
     borderWidth: "2px",
   };
+
+  // console.log(context.rooms);
+  // console.log(context.rooms[0].room_id);
 
   function filterSearchResult() {
     //เทียบข้อมูล user กับข้อมูลห้องทั้งหมด(rooms) แล้วคืนค่าเป็น array ห้องที่พักได้
@@ -24,11 +30,22 @@ function SearchResult() {
         <Search buttonStyle={buttonStyle} />
       </div>
       <div className="bg-bg  flex flex-col items-center pt-[90px] pb-[300px] px-[100px]">
-        //เอา array มา map
-        <RoomResultCard />
-        {/* <RoomResultCard />
-        <RoomResultCard />
-        <RoomResultCard /> */}
+        {/* //เอา array มา map */}
+        {context.rooms.map((room) => (
+          <RoomResultCard
+            roomId={room.room_id}
+            roomType={room.room_type}
+            roomImages={room.room_images}
+            bedType={room.bed_types}
+            description={room.description}
+            area={room.area}
+            price={room.price}
+            promotionPrice={room.promotion_price}
+            amenity={room.amenity}
+            person={room.person}
+            available={room.available}
+          />
+        ))}
       </div>
       <Footer />
     </div>
