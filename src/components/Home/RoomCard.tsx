390cd290971b24f79c650ca53340ab14b2365127
unknown <ratiporn.pr@gmail.com>
2023-09-08 00:38:04 +0700

    Feat: add default search result

--- src/components/Home/RoomCard.tsx
+++ src/components/Home/RoomCard.tsx
@@ -23,7 +23,7 @@ function RoomCard({
   // console.log(room);
   // console.log(room.room_images[0]);
 
-  const style = {
+  const backgroundImage = {
     display: "flex",
     backgroundSize: "cover",
     backgroundPosition: "center",
@@ -35,7 +35,7 @@ function RoomCard({
     // 'https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/images/superior-graden-2?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pbWFnZXMvc3VwZXJpb3ItZ3JhZGVuLTIiLCJpYXQiOjE2OTM4MzUxMTksImV4cCI6MTcyNTM3MTExOX0.NCux_pItpHFf9WqMaPDsjukXHFofhBnpjglVo5aGJng&t=2023-09-04T13%3A45%3A17.758Z'
     // {`flex bg-cover bg-center bg-[url('${room.room_images[0]}')]`}
     // "flex bg-cover bg-center bg-[url('https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/images/superior-graden-2?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pbWFnZXMvc3VwZXJpb3ItZ3JhZGVuLTIiLCJpYXQiOjE2OTM4MzUxMTksImV4cCI6MTcyNTM3MTExOX0.NCux_pItpHFf9WqMaPDsjukXHFofhBnpjglVo5aGJng&t=2023-09-04T13%3A45%3A17.758Z')]"
-    <div style={style}>
+    <div style={backgroundImage}>
       <div className="w-full bg-black bg-opacity-30 pl-16 pb-20 flex items-end justify-start">
         <div className="z-10">
           <div className="mb-3 font-noto-serif-display text-headline3 text-white">
