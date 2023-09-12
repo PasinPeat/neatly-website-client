1202673050b379d6e009eea6423cf8b5d1d62655
Kittiwat Choocheep <KyouKung.000@gmail.com>
2023-09-12 11:39:38 +0700

    feat: add validation payment

--- src/pages/PaymentMethod.tsx
+++ src/pages/PaymentMethod.tsx
@@ -5,9 +5,16 @@ import axios from "axios";
 
 interface RouteParams {
   paymentmethodID: string;
+  [key: string]: string | undefined;
 }
 
 function PaymentMethod() {
+  const [fullNameErrorCredit, setFullNameErrorCredit] = useState(false);
+  const [creditCardError, setCreditCardError] = useState(false);
+
+  // //loading
+  const [isLoading, setIsLoading] = useState(false);
+
   const params = useParams<RouteParams>();
   const [payment, setPayment] = useState({
     card_number: "",
@@ -16,16 +23,40 @@ function PaymentMethod() {
     cvc: "",
   });
 
+  // check credit name handler
+  const validateFullNameCredit = (name: string) => {
+    const names = name.trim().split(" ");
+    if (
+      names.length !== 2 ||
+      !/^[a-zA-Z]*$/.test(names[0]) ||
+      !/^[a-zA-Z]*$/.test(names[1])
+    ) {
+      setFullNameErrorCredit(true);
+    } else {
+      setFullNameErrorCredit(false);
+    }
+  };
+
   const getPaymentID = async () => {
-    try {
-      const response = await axios.get(
-        `http://localhost:4000/paymentmethod/${params.paymentmethodID}`
-      );
-      console.log(response.data.data);
-      const data = response.data.data;
-      setPayment(data);
-    } catch (error) {
-      console.error(error);
+    if (!fullNameErrorCredit && !creditCardError) {
+      try {
+        const response = await axios.get(
+          `http://localhost:4000/paymentmethod/${params.paymentmethodID}`
+        );
+        console.log(response.data.data);
+        const data = response.data.data;
+
+        const formattedCardNumber = data.card_number
+          .replace(/[^\d]/g, "")
+          .replace(/(\d{4})/g, "$1 ")
+          .trim();
+        setPayment({
+          ...data,
+          card_number: formattedCardNumber,
+        });
+      } catch (error) {
+        console.error(error);
+      }
     }
   };
 
@@ -33,25 +64,77 @@ function PaymentMethod() {
     getPaymentID();
   }, [params.paymentmethodID]);
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault();
-    try {
-      const response = await axios.put(
-        `http://localhost:4000/paymentmethod/${params.paymentmethodID}`,
-        payment
-      );
-      console.log(response.data);
-    } catch (error) {
-      console.error(error);
+
+    setIsLoading(true);
+
+    if (!fullNameErrorCredit && !creditCardError) {
+      try {
+        const cleanedCardNumber = payment.card_number.replace(/\s/g, "");
+        const response = await axios.put(
+          `http://localhost:4000/paymentmethod/${params.paymentmethodID}`,
+          { ...payment, card_number: cleanedCardNumber }
+        );
+        console.log(response.data);
+        setIsLoading(false);
+        window.location.reload();
+      } catch (error) {
+        console.error(error);
+        setIsLoading(false);
+      }
+    } else {
+      setIsLoading(false);
+      return;
     }
   };
 
-  const handleChange = (e) => {
+  const handleChange = (e: any) => {
     const { name, value } = e.target;
     setPayment({
       ...payment,
       [name]: value,
     });
+
+    if (name === "card_number") {
+      let formattedValue = value
+        .replace(/[^\d]/g, "")
+        .replace(/(\d{4})/g, "$1 ")
+        .trim();
+
+      if (formattedValue.length > 19) {
+        return;
+      } else if (formattedValue.length < 19) {
+        setCreditCardError(true);
+      } else {
+        setCreditCardError(false);
+      }
+
+      setPayment({
+        ...payment,
+        [name]: formattedValue,
+      });
+    }
+
+    if (name === "expire_date") {
+      const cleanedValue = e.target.value.replace(/\D/g, "");
+      const formattedValue = cleanedValue.replace(
+        /^(\d{2})(\d{0,2})/,
+        (_: any, month: any, year: any) => {
+          const maxMonth = month > 12 ? "12" : month;
+          return `${maxMonth}${year ? "/" : ""}${year}`;
+        }
+      );
+
+      setPayment({
+        ...payment,
+        [name]: formattedValue,
+      });
+    }
+
+    if (name === "card_owner") {
+      validateFullNameCredit(value);
+    }
   };
 
   return (
@@ -63,15 +146,24 @@ function PaymentMethod() {
             <h1 className="font-noto-serif-display text-[68px] font-medium text-green-800">
               Payment Method
             </h1>
-            <button className="btn Button w-[258px] h-[48px]" type="submit">
-              Update Payment Method
+            <button
+              className="btn Button w-[258px] h-[48px]"
+              type="submit"
+              disabled={isLoading}
+            >
+              {isLoading ? (
+                <span className="loading loading-spinner w-7 bg-orange-600"></span>
+              ) : (
+                "Update Payment Method"
+              )}
             </button>
           </div>
           <p className="text-headline5 mt-[58px] mb-[38px] text-gray-600">
             Credit Card
           </p>
+
           <div className="grid grid-rows-2 grid-flow-col gap-x-[22px] mb-[38px]">
-            <div>
+            <div className="relative">
               <label htmlFor="cardNumber">
                 <p className="font-body1 text-gray-900  mb-[4px]">
                   Card Number
@@ -82,11 +174,28 @@ function PaymentMethod() {
                 type="text"
                 name="card_number"
                 placeholder="Enter your card number"
-                className="w-full Input mb-[38px] text-black"
+                className={`w-full mb-[38px] text-black Input focus:outline-none focus:border-orange-500 ${
+                  creditCardError ? "border-[#B61515]" : "focus:outline-none"
+                }`}
                 value={payment.card_number}
                 onChange={handleChange}
+                maxLength={19}
                 required
               />
+              {creditCardError && (
+                <>
+                  <div className="absolute right-5 top-[53px] transform -translate-y-1/2">
+                    <img
+                      src="https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/icon/123.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pY29uLzEyMy5zdmciLCJpYXQiOjE2OTQxNjg2ODAsImV4cCI6MTcyNTcwNDY4MH0.p_CkT1rbBXUoggWNacczlqIqq5lHcnphLnOHkPXlDTQ&t=2023-09-08T10%3A24%3A33.645Z"
+                      alt="Error Icon"
+                      className="h-5 w-5"
+                    />
+                  </div>
+                  <p className="text-body3 text-red absolute top-[85px]">
+                    Invalid Credit Card number.
+                  </p>
+                </>
+              )}
             </div>
             <div>
               <label htmlFor="expire_date">
@@ -99,13 +208,14 @@ function PaymentMethod() {
                 type="tel"
                 name="expire_date"
                 placeholder="MM/YY"
-                className="w-full Input mb-[38px] text-black"
+                maxLength={5}
+                className="w-full Input mb-[38px] text-black focus:outline-none focus:border-orange-500"
                 value={payment.expire_date}
                 onChange={handleChange}
                 required
               />
             </div>
-            <div>
+            <div className="relative">
               <label htmlFor="cardOwner">
                 <p className="font-body1 text-gray-900  mb-[4px]">Card Owner</p>
               </label>
@@ -114,11 +224,30 @@ function PaymentMethod() {
                 type="text"
                 name="card_owner"
                 placeholder="Enter your name"
-                className="w-full Input mb-[38px] text-black"
+                className={`w-full mb-[38px] text-black Input focus:outline-none focus:border-orange-500 ${
+                  fullNameErrorCredit
+                    ? "border-[#B61515]"
+                    : "focus:outline-none"
+                }`}
                 value={payment.card_owner}
                 onChange={handleChange}
                 required
               />
+              {fullNameErrorCredit && (
+                <>
+                  <div className="absolute right-5 top-[53px] transform -translate-y-1/2">
+                    <img
+                      src="https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/icon/123.svg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9pY29uLzEyMy5zdmciLCJpYXQiOjE2OTQxNjg2ODAsImV4cCI6MTcyNTcwNDY4MH0.p_CkT1rbBXUoggWNacczlqIqq5lHcnphLnOHkPXlDTQ&t=2023-09-08T10%3A24%3A33.645Z"
+                      alt="Error Icon"
+                      className="h-5 w-5"
+                    />
+                  </div>
+                  <p className="text-body3 text-red absolute top-[85px]">
+                    Card Owner's name should include both first name and last
+                    name.
+                  </p>
+                </>
+              )}
             </div>
 
             <div>
@@ -130,8 +259,9 @@ function PaymentMethod() {
                 type="tel"
                 name="cvc"
                 pattern="\d*"
+                maxLength={3}
                 placeholder="CVC/CVV"
-                className="w-full Input mb-[38px] text-black"
+                className="w-full Input mb-[38px] text-black focus:outline-none focus:border-orange-500"
                 value={payment.cvc}
                 onChange={handleChange}
                 required
