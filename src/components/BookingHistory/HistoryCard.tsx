b6b3bff17b7b26569e0d64de764af5b05667d45f
Tongcnc <tongcnc3@gmail.com>
2023-09-18 14:20:20 +0700

    fix: merge conflict

--- src/components/BookingHistory/HistoryCard.tsx
+++ src/components/BookingHistory/HistoryCard.tsx
@@ -82,165 +82,35 @@ function HistoryCard({
       <div className="flex flex-col items-center w-full bg-bg text-gray-700">
         <div className=" flex flex-col pt-10 border-b-[1px] border-gray-300">
           <div className="w-[1120px] flex justify-between">
-            <div className="">
-              <div className="w-[357px] h-[210px] rounded bg-cover bg-center">
-                <img src="https://kewjjbauwpznfmeqbdpp.supabase.co/storage/v1/object/sign/dev-storage/room_images/04%20Supreme/K-Studio_Lambs_Lions_CasaCookChania_022_GeorgRoske.jpg?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJkZXYtc3RvcmFnZS9yb29tX2ltYWdlcy8wNCBTdXByZW1lL0stU3R1ZGlvX0xhbWJzX0xpb25zX0Nhc2FDb29rQ2hhbmlhXzAyMl9HZW9yZ1Jvc2tlLmpwZyIsImlhdCI6MTY5NDUwODA0NSwiZXhwIjoxNzI2MDQ0MDQ1fQ.GPUAPR5qHYjoK9TqISz_AlCFcWJR0gvKKpu4gFkRw9k&t=2023-09-12T08%3A40%3A44.773Z" />
+            <div
+              style={backgroundImage}
+              className="w-[357px] h-[210px] rounded bg-cover bg-center"
+            ></div>
+            <div className="flex flex-col py-6 justify-between w-[715px]">
+              <div className="flex flex-row justify-between items-center mb-5">
+                <h2 className="text-headline4 text-black">{roomType}</h2>
+                <p className="text-body1">
+                  Booking date: <span>{formattedBookDate}</span>
+                </p>
               </div>
-            </div>
-            <div className="flex flex-col py-6 justify-between">
-              <div className="w-[715px]">
-                <div className="flex flex-row justify-between items-center mb-5">
-                  <h2 className="text-headline4 text-black">
-                    Superior Garden View
-                  </h2>
-                  <p className="text-body1">Booking date: Tue, 16 Oct 2022</p>
-                </div>
-                <div className=" flex gap-10">
-                  <div className="flex flex-col gap-1 ">
-                    <p className=" font-bold text-grey-800">Check-in</p>
-                    <div>
-                      <span>Th, 19 Oct 2022 </span>
-                      <span>|</span>
-                      <span> After 2:00 PM</span>
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <p className="font-bold text-grey-800">Check-in</p>
-                    <div>
-                      <span>Th, 19 Oct 2022 </span>
-                      <span>|</span>
-                      <span> After 2:00 PM</span>
-                    </div>
+              <div className=" flex gap-10">
+                <div className="flex flex-col gap-1">
+                  <p className=" font-bold text-grey-800">Check-in</p>
+                  <div>
+                    <span>{formattedCheckIn}</span>
+                    <span> |</span>
+                    {/* fix: fix to fetch *After* from detabase */}
+                    <span> After 2:00 PM</span>
                   </div>
                 </div>
-                <div className=" pt-5 ">
-                  <List
-                    sx={{ width: "100%", bgcolor: "#F1F2F6" }}
-                    component="nav"
-                    aria-labelledby="nested-list-subheader"
-                    // subheader={
-                    //   <ListSubheader
-                    //     component="div"
-                    //     id="nested-list-subheader"
-                    //     className=" text-headline3"
-                    //   >
-                    //     Booking Detail
-                    //   </ListSubheader>
-                    // }
-                  >
-                    <ListItemButton onClick={handleClick} sx={{ px: 4 }}>
-                      <p className="font-bold text-grey-800">Booking Detail</p>
-                      {/* <ListItemIcon>
-                        <InboxIcon />
-                      </ListItemIcon> */}
-                      <ListItemText primary="" />
-
-                      {open ? <ExpandLess /> : <ExpandMore />}
-                    </ListItemButton>
-                    <Collapse
-                      in={open}
-                      timeout="auto"
-                      unmountOnExit
-                      className=" px-3"
-                    >
-                      <List
-                        component="div"
-                        disablePadding
-                        sx={{ px: 2, py: 1 }}
-                      >
-                        <div className="flex justify-between w-full py-4">
-                          <p className="text-body1">2 Guest (1 Night)</p>
-                          <div className="flex">
-                            <p className="text-body1">Payment usccess via</p>
-                            <span className=" font-bold pl-1">
-                              Credit Card-*888
-                            </span>
-                          </div>
-                        </div>
-                        {/* <ListItemText primary="Pay" />   */}
-                      </List>
-                      <List
-                        component="div"
-                        disablePadding
-                        sx={{ px: 2, py: 1 }}
-                      >
-                        <div className="flex justify-between w-full">
-                          <p className="text-body1">
-                            Superior Garden View Room
-                          </p>
-
-                          <p className="text-body1 font-bold text-black">
-                            2,500.00
-                          </p>
-                        </div>
-                        {/* <ListItemText primary="Pay" />   */}
-                      </List>
-                      <List
-                        component="div"
-                        disablePadding
-                        sx={{ px: 2, py: 1 }}
-                      >
-                        <div className="flex justify-between w-full">
-                          <p className="text-body1">Airport tranfer</p>
-
-                          <p className="text-body1 font-bold text-black">
-                            200.00
-                          </p>
-                        </div>
-                        {/* <ListItemText primary="Pay" />   */}
-                      </List>
-                      <List
-                        component="div"
-                        disablePadding
-                        sx={{ px: 2, py: 1 }}
-                      >
-                        <div className="flex justify-between w-full">
-                          <p className="text-body1">Promotion Code</p>
-
-                          <p className="text-body1 font-bold text-black">
-                            -400.00
-                          </p>
-                        </div>
-                        {/* <ListItemText primary="Pay" />   */}
-                      </List>
-                      <List
-                        component="div"
-                        disablePadding
-                        sx={{ px: 2, py: 1 }}
-                      >
-                        <div className="flex justify-between w-full border-t-[2px] border-green-300 py-4">
-                          <p className="text-body1 ">Total</p>
-
-                          <p className=" text-headline5 font-bold text-black">
-                            THB 2,300.00
-                          </p>
-                          {/* {price.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "THB",
-                  })} */}
-                        </div>
-                        {/* <ListItemText primary="Pay" />   */}
-                      </List>
-                      <List
-                        component="div"
-                        disablePadding
-                        sx={{ px: 2, py: 1, bgcolor: "#E4E6ED" }}
-                      >
-                        <div className="flex flex-col justify-between w-full py-4">
-                          <p className="text-body1 font-bold">
-                            Additional Request
-                          </p>
-
-                          <p className=" text-body1 pt-2">THB 2,300.00</p>
-                          {/* {price.toLocaleString("en-US", {
-                    style: "currency",
-                    currency: "THB",
-                  })} */}
-                        </div>
-                        {/* <ListItemText primary="Pay" />   */}
-                      </List>
-                    </Collapse>
-                  </List>
+                <div className="flex flex-col gap-1">
+                  <p className="font-bold text-grey-800">Check-out</p>
+                  <div>
+                    <span>{formattedCheckOut}</span>
+                    <span> |</span>
+                    {/* fix: fix to fetch *Before* from detabase */}
+                    <span> Before 12:00 PM</span>
+                  </div>
                 </div>
               </div>
               <DropDownList
