152a53d36850617abc2e333d60af1b8d10c160b8
PasinPeat <pasin.lawn1995@gmail.com>
2023-09-21 11:45:49 +0700

    Feat:create admin page2

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -2,9 +2,11 @@ import CustomerBooking from '../components/Admin/CustomerBooking'
 import HotelInformation from '../components/Admin/HotelInformation'
 import RoomAndProperty from '../components/Admin/RoomAndProperty'
 import RoomManagement from '../components/Admin/RoomManagement'
+import Sidebar from '../components/Admin/Sidebar'
 function Admin() {
   return (
     <>
+    <Sidebar/>
     <CustomerBooking/>
     <HotelInformation/>
     <RoomAndProperty/>
