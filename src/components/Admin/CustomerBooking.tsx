b107ceeaef328bca8689547c8bb0d18c108925ed
unknown <ratiporn.pr@gmail.com>
2023-09-22 16:16:48 +0700

    fix: rebase

--- src/components/Admin/CustomerBooking.tsx
+++ src/components/Admin/CustomerBooking.tsx
@@ -1,9 +1,79 @@
-
-
 function CustomerBooking() {
   return (
-    <div>CustomerBooking</div>
-  )
+    <div className="bg-gray-100 w-screen py-16 px-12">
+      <div className="overflow-x-auto bg-white ">
+        <table className="table table-lg">
+          <thead className="bg-gray-300 text-body2">
+            <tr>
+              <th>Customer name</th>
+              <th>Guest(s)</th>
+              <th>Room type</th>
+              <th>Amount</th>
+              <th>Bed type</th>
+              <th>Check-in</th>
+              <th>Check-out</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Kate Cho</td>
+              <td>2</td>
+              <td>Superior Garden View</td>
+              <td>1</td>
+              <td>Single Bed</td>
+              <td>Th, 19 Oct 2022</td>
+              <td>Th, 19 Oct 2022</td>
+            </tr>
+            <tr>
+              <td>Kate Cho</td>
+              <td>2</td>
+              <td>Superior Garden View</td>
+              <td>1</td>
+              <td>Single Bed</td>
+              <td>Th, 19 Oct 2022</td>
+              <td>Th, 19 Oct 2022</td>
+            </tr>
+            <tr>
+              <td>Kate Cho</td>
+              <td>2</td>
+              <td>Superior Garden View</td>
+              <td>1</td>
+              <td>Single Bed</td>
+              <td>Th, 19 Oct 2022</td>
+              <td>Th, 19 Oct 2022</td>
+            </tr>
+            <tr>
+              <td>Kate Cho</td>
+              <td>2</td>
+              <td>Superior Garden View</td>
+              <td>1</td>
+              <td>Single Bed</td>
+              <td>Th, 19 Oct 2022</td>
+              <td>Th, 19 Oct 2022</td>
+            </tr>
+            <tr>
+              <td>Kate Cho</td>
+              <td>2</td>
+              <td>Superior Garden View</td>
+              <td>1</td>
+              <td>Single Bed</td>
+              <td>Th, 19 Oct 2022</td>
+              <td>Th, 19 Oct 2022</td>
+            </tr>
+            <tr>
+              <td>Kate Cho</td>
+              <td>2</td>
+              <td>Superior Garden View</td>
+              <td>1</td>
+              <td>Single Bed</td>
+              <td>Th, 19 Oct 2022</td>
+              <td>Th, 19 Oct 2022</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
 
-export default CustomerBooking
+export default CustomerBooking;
