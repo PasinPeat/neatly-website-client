dffb246987472075273bfd0ae768f29e423e2192
unknown <ratiporn.pr@gmail.com>
2023-09-25 12:30:35 +0700

    feat: add pagination in custom booking

--- src/components/Admin/CustomerBooking.tsx
+++ src/components/Admin/CustomerBooking.tsx
@@ -1,61 +1,110 @@
 import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import Paper from "@mui/material/Paper";
 import SearchIcon from "@mui/icons-material/Search";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import FormControl from "@mui/material/FormControl";
 import InputAdornment from "@mui/material/InputAdornment";
-import { ClassNames } from "@emotion/react";
+import TablePagination from "@mui/material/TablePagination";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import React from "react";
 import { useState, useEffect } from "react";
+import { useTheme } from "@mui/material/styles";
 import axios from "axios";
 import dayjs from "dayjs";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#E4E6ED",
-    fontSize: 14,
-    fontWeight: 500,
-    fontFamily: "Inter",
-    color: "#424C6B",
-    padding: "10px 16px",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    backgroundColor: "white",
-    fontSize: 16,
-    fontFamily: "Inter",
-    color: "black",
-    borderColor: "#E4E6ED",
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-function createData(
-  customerName: string,
-  guest: number,
-  roomType: string,
-  amount: number,
-  bedType: string,
-  checkIn: string,
-  checkOut: string
-) {
-  return { customerName, guest, roomType, amount, bedType, checkIn, checkOut };
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number
+  ) => void;
 }
 
-function CustomerBooking() {
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+export default function CustomPaginationActionsTable() {
   const [booking, setBooking] = useState([]);
 
   const getBooking = async () => {
@@ -73,7 +122,11 @@ function CustomerBooking() {
     getBooking();
   }, []);
 
+  let isCancelled;
+
   const rows = booking.map((book) => {
+    isCancelled = book.status === "cancel";
+
     return createData(
       book.users.fullName,
       book.amount_stay,
@@ -81,69 +134,170 @@ function CustomerBooking() {
       book.amount_room,
       book.room_details.bed_types,
       book.check_in,
-      book.check_out
+      book.check_out,
+      isCancelled
     );
   });
 
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#E4E6ED",
+      fontSize: 14,
+      fontWeight: 500,
+      fontFamily: "Inter",
+      color: "#424C6B",
+      padding: "10px 16px",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      backgroundColor: "white",
+      fontSize: 16,
+      fontFamily: "Inter",
+      color: "black",
+      borderColor: "#E4E6ED",
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme, isCancelled }) => ({
+    backgroundColor: isCancelled ? "#000000" : theme.palette.action.hover,
+    "&:nth-of-type(odd)": {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  function createData(
+    customerName: string,
+    guest: number,
+    roomType: string,
+    amount: number,
+    bedType: string,
+    checkIn: string,
+    checkOut: string
+  ) {
+    return {
+      customerName,
+      guest,
+      roomType,
+      amount,
+      bedType,
+      checkIn,
+      checkOut,
+    };
+  }
+
   return (
     <div>
-      <div className="bg-white h-20 flex flex-row justify-between items-center drop-shadow-md px-16">
-        <p className=" text-black font-bold">Customer Booking</p>
-        <div>
-          <FormControl>
-            <OutlinedInput
-              placeholder="Search…"
-              size="small"
-              id="input-with-icon-adornment"
-              inputProps={{
-                "aria-label": "weight",
-              }}
-              startAdornment={
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              }
-            />
-          </FormControl>
+      <div className="bg-gray-100 h-screen">
+        <div className="bg-white h-20 flex flex-row justify-between items-center drop-shadow-md px-16">
+          <p className=" text-black font-bold">Customer Booking</p>
+          <div>
+            <FormControl>
+              <OutlinedInput
+                placeholder="Search…"
+                size="small"
+                id="input-with-icon-adornment"
+                inputProps={{
+                  "aria-label": "weight",
+                }}
+                startAdornment={
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
         </div>
-      </div>
-      {/* table field*/}
-      <div className="bg-gray-100 px-16 py-12">
-        <Paper sx={{ overflow: "hidden" }}>
-          <TableContainer component={Paper}>
-            <Table sx={{ maxHeight: 1000 }} aria-label="customized table ">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Customer</StyledTableCell>
-                  <StyledTableCell>Guest(s)</StyledTableCell>
-                  <StyledTableCell>Room type</StyledTableCell>
-                  <StyledTableCell>Amount</StyledTableCell>
-                  <StyledTableCell>Bed type</StyledTableCell>
-                  <StyledTableCell>Check-in</StyledTableCell>
-                  <StyledTableCell>Check-out</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow>
-                    <StyledTableCell component="th" scope="row">
-                      {row.customerName}
-                    </StyledTableCell>
-                    <StyledTableCell>{row.guest}</StyledTableCell>
-                    <StyledTableCell>{row.roomType}</StyledTableCell>
-                    <StyledTableCell>{row.amount}</StyledTableCell>
-                    <StyledTableCell>{row.bedType}</StyledTableCell>
-                    <StyledTableCell>{row.checkIn}</StyledTableCell>
-                    <StyledTableCell>{row.checkOut}</StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        </Paper>
+
+        {/* table field*/}
+        <div className="px-24 py-12">
+          <Paper sx={{ overflow: "hidden" }}>
+            <TableContainer component={Paper}>
+              <Table aria-label="custom pagination table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Customer</StyledTableCell>
+                    <StyledTableCell>Guest(s)</StyledTableCell>
+                    <StyledTableCell>Room type</StyledTableCell>
+                    <StyledTableCell>Amount</StyledTableCell>
+                    <StyledTableCell>Bed type</StyledTableCell>
+                    <StyledTableCell>Check-in</StyledTableCell>
+                    <StyledTableCell>Check-out</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {(rowsPerPage > 0
+                    ? rows.slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                    : rows
+                  ).map((row) => (
+                    <StyledTableRow isCancelled={isCancelled}>
+                      <StyledTableCell>{row.customerName}</StyledTableCell>
+                      <StyledTableCell>{row.guest}</StyledTableCell>
+                      <StyledTableCell>{row.roomType}</StyledTableCell>
+                      <StyledTableCell>{row.amount}</StyledTableCell>
+                      <StyledTableCell>{row.bedType}</StyledTableCell>
+                      <StyledTableCell>{row.checkIn}</StyledTableCell>
+                      <StyledTableCell>{row.checkOut}</StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[10]}
+                      colSpan={3}
+                      count={rows.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: {
+                          "aria-label": "rows per page",
+                        },
+                        native: true,
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </TableContainer>
+          </Paper>
+        </div>
       </div>
     </div>
   );
 }
-
-export default CustomerBooking;
