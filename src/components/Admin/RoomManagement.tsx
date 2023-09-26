10595c72c908155d396649792b01e077ef303768
Tongcnc <tongcnc3@gmail.com>
2023-09-26 13:24:23 +0700

    feat: add pagination

--- src/components/Admin/RoomManagement.tsx
+++ src/components/Admin/RoomManagement.tsx
@@ -6,11 +6,13 @@ import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import Paper from "@mui/material/Paper";
 import SearchIcon from "@mui/icons-material/Search";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import FormControl from "@mui/material/FormControl";
 import InputAdornment from "@mui/material/InputAdornment";
+import TablePagination from "@mui/material/TablePagination";
 import IconButton from "@mui/material/IconButton";
 import FirstPageIcon from "@mui/icons-material/FirstPage";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
@@ -21,85 +23,85 @@ import { useTheme } from "@mui/material/styles";
 import axios from "axios";
 import DropdownSearch from "./DropdownSearch";
 
-// interface TablePaginationActionsProps {
-//   count: number;
-//   page: number;
-//   rowsPerPage: number;
-//   onPageChange: (
-//     event: React.MouseEvent<HTMLButtonElement>,
-//     newPage: number
-//   ) => void;
-// }
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number
+  ) => void;
+}
 
-// function TablePaginationActions(props: TablePaginationActionsProps) {
-//   const theme = useTheme();
-//   const { count, page, rowsPerPage, onPageChange } = props;
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
 
-//   const handleFirstPageButtonClick = (
-//     event: React.MouseEvent<HTMLButtonElement>
-//   ) => {
-//     onPageChange(event, 0);
-//   };
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, 0);
+  };
 
-//   const handleBackButtonClick = (
-//     event: React.MouseEvent<HTMLButtonElement>
-//   ) => {
-//     onPageChange(event, page - 1);
-//   };
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page - 1);
+  };
 
-//   const handleNextButtonClick = (
-//     event: React.MouseEvent<HTMLButtonElement>
-//   ) => {
-//     onPageChange(event, page + 1);
-//   };
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page + 1);
+  };
 
-//   const handleLastPageButtonClick = (
-//     event: React.MouseEvent<HTMLButtonElement>
-//   ) => {
-//     onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-//   };
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
 
-//   return (
-//     <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-//       <IconButton
-//         onClick={handleFirstPageButtonClick}
-//         disabled={page === 0}
-//         aria-label="first page"
-//       >
-//         {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-//       </IconButton>
-//       <IconButton
-//         onClick={handleBackButtonClick}
-//         disabled={page === 0}
-//         aria-label="previous page"
-//       >
-//         {theme.direction === "rtl" ? (
-//           <KeyboardArrowRight />
-//         ) : (
-//           <KeyboardArrowLeft />
-//         )}
-//       </IconButton>
-//       <IconButton
-//         onClick={handleNextButtonClick}
-//         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-//         aria-label="next page"
-//       >
-//         {theme.direction === "rtl" ? (
-//           <KeyboardArrowLeft />
-//         ) : (
-//           <KeyboardArrowRight />
-//         )}
-//       </IconButton>
-//       <IconButton
-//         onClick={handleLastPageButtonClick}
-//         disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-//         aria-label="last page"
-//       >
-//         {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-//       </IconButton>
-//     </Box>
-//   );
-// }
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
 
 export default function CustomPaginationActionsTable() {
   const [booking, setBooking] = useState([]);
@@ -161,10 +163,10 @@ export default function CustomPaginationActionsTable() {
   let isCancelled;
 
   const rows = filterBookingList.map((book) => {
-    isCancelled = filterBookingList.status === "cancel";
+    isCancelled = book.status === "cancel";
 
     return createData(
-      book.room_details.room_id,
+      book.room_avaliable_id,
       book.room_details.room_type,
       book.room_details.bed_types,
       null,
@@ -172,26 +174,26 @@ export default function CustomPaginationActionsTable() {
     );
   });
 
-  // const [page, setPage] = React.useState(0);
-  // const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
   // Avoid a layout jump when reaching the last page with empty rows.
-  // const emptyRows =
-  //   page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
 
-  // const handleChangePage = (
-  //   event: React.MouseEvent<HTMLButtonElement> | null,
-  //   newPage: number
-  // ) => {
-  //   setPage(newPage);
-  // };
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
 
-  // const handleChangeRowsPerPage = (
-  //   event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  // ) => {
-  //   setRowsPerPage(parseInt(event.target.value, 10));
-  //   setPage(0);
-  // };
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
 
   const StyledTableCell = styled(TableCell)(({ theme }) => ({
     [`&.${tableCellClasses.head}`]: {
@@ -228,8 +230,6 @@ export default function CustomPaginationActionsTable() {
     return { roomNumber, roomType, bedType, status };
   }
 
-  // const rows = [createData(2, "Superior Garden View", "Single Bed", "status")];
-  // function RoomManagement() {
   return (
     <>
       <div className=" w-full">
@@ -258,31 +258,64 @@ export default function CustomPaginationActionsTable() {
           </div>
         </div>
         {/* table field*/}
-        <div className="bg-gray-100 w-full py-16 px-12">
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Room no.</StyledTableCell>
-                  <StyledTableCell>Room type</StyledTableCell>
-                  <StyledTableCell>Bed type</StyledTableCell>
-                  <StyledTableCell>Status</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow>
-                    <StyledTableCell>000{row.roomNumber}</StyledTableCell>
-                    <StyledTableCell>{row.roomType}</StyledTableCell>
-                    <StyledTableCell>{row.bedType}</StyledTableCell>
-                    <StyledTableCell>
-                      <DropdownSearch />
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <div className="px-24 py-12">
+          <Paper sx={{ overflow: "hidden" }}>
+            <TableContainer component={Paper}>
+              <Table aria-label="custom pagination table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell>Room no.</StyledTableCell>
+                    <StyledTableCell>Room type</StyledTableCell>
+                    <StyledTableCell>Bed type</StyledTableCell>
+                    <StyledTableCell>Status</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {(rowsPerPage > 0
+                    ? rows.slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                    : rows
+                  ).map((row, index) => (
+                    <StyledTableRow key={index} isCancelled={row.isCancelled}>
+                      <StyledTableCell>{row.roomNumber}</StyledTableCell>
+                      <StyledTableCell>{row.roomType}</StyledTableCell>
+                      <StyledTableCell>{row.bedType}</StyledTableCell>
+                      <StyledTableCell>
+                        <DropdownSearch />
+                      </StyledTableCell>
+                    </StyledTableRow>
+                  ))}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[10]}
+                      colSpan={3}
+                      count={rows.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: {
+                          "aria-label": "rows per page",
+                        },
+                        native: true,
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </TableContainer>
+          </Paper>
         </div>
       </div>
     </>
