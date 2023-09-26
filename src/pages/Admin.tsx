3bab76ac73a79c6741c1f77b7cfe0cb877f90d6d
PasinPeat <pasin.lawn1995@gmail.com>
2023-09-26 22:06:12 +0700

    fix:fix sidebar position

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -80,8 +80,8 @@ function Admin() {
 
   return (
     <>
-      <div className="h-screen w-screen relative">
-        <div className="w-[230px] h-full fixed overflow-x-hidden overflow-y-hidden top-0 left-0 z-50 bg-green-800 flex flex-col items-center ">
+      <div className="h-screen w-screen overflow-x-hidden">
+        <div className="w-[240px] h-full fixed overflow-y-hidden top-0 left-0 z-50 bg-green-800 flex flex-col items-center ">
           <Drawer variant="permanent" open={open} sx={DrawerSx}>
             <div className="bg-green-800 flex flex-col items-center pb-[70px]">
               <img
@@ -106,7 +106,7 @@ function Admin() {
             </List>
           </Drawer>
         </div>
-        <div className="w-[85%] ml-[230px] overflow-x-hidden">
+        <div className="w-[100vw-240px] ml-[240px]">
           {customerBooking && <CustomerBooking />}
           {roomManage && <RoomManagement />}
           {hotelInfo && <HotelInformation />}
