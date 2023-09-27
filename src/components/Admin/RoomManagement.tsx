8e5c8656f40322783ad472c8e6f45889b4c09992
Tongcnc <tongcnc3@gmail.com>
2023-09-27 23:47:33 +0700

    feat: room management

--- src/components/Admin/RoomManagement.tsx
+++ src/components/Admin/RoomManagement.tsx
@@ -1,79 +1,256 @@
 import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell, { tableCellClasses } from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import Paper from "@mui/material/Paper";
 import SearchIcon from "@mui/icons-material/Search";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import FormControl from "@mui/material/FormControl";
 import InputAdornment from "@mui/material/InputAdornment";
+import TablePagination from "@mui/material/TablePagination";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import React, { useState, useEffect } from "react";
+import { useTheme } from "@mui/material/styles";
+import axios from "axios";
+import DropdownSearch from "./DropdownSearch";
 
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: "#E4E6ED",
-    fontSize: 14,
-    fontWeight: 500,
-    fontFamily: "Inter",
-    color: "#424C6B",
-    padding: "10px 16px",
-  },
-  [`&.${tableCellClasses.body}`]: {
-    backgroundColor: "white",
-    fontSize: 16,
-    fontFamily: "Inter",
-    color: "black",
-    borderColor: "#E4E6ED",
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-function createData(
-  customerName: string,
-  guest: number,
-  roomType: string,
-  amount: number,
-  bedType: string,
-  checkIn: string,
-  checkOut: string
-) {
-  return { customerName, guest, roomType, amount, bedType, checkIn, checkOut };
+interface TablePaginationActionsProps {
+  count: number;
+  page: number;
+  rowsPerPage: number;
+  onPageChange: (
+    event: React.MouseEvent<HTMLButtonElement>,
+    newPage: number
+  ) => void;
 }
 
-const rows = [
-  createData(
-    "Kate Cho",
-    2,
-    "Superior Garden View",
-    1,
-    "Single Bed",
-    "Th, 19 Oct 2022",
-    "Th, 19 Oct 2022"
-  ),
-];
-function RoomManagement() {
+function TablePaginationActions(props: TablePaginationActionsProps) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (
+    event: React.MouseEvent<HTMLButtonElement>
+  ) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+interface BookingType {
+  room_avaliable_id: number;
+  room_details: {
+    room_type: string;
+    bed_types: string;
+  };
+  room_status: string;
+}
+
+export default function CustomPaginationActionsTable() {
+  const [booking, setBooking] = useState<BookingType[]>([]);
+  const [filterBookingList, setFilterBookingList] = useState<BookingType[]>([]);
+  const [selectedByText, setSelectedByText] = useState<string>("");
+
+  const getBooking = async () => {
+    try {
+      const results = await axios(
+        `http://localhost:4000/avaliable/admin/admin`
+      );
+
+      setBooking(results.data);
+      setFilterBookingList(results.data);
+      console.log(results.data);
+    } catch (error) {
+      console.error("Error fetching room data:", error);
+    }
+  };
+
+  const pattern = new RegExp(selectedByText, "i");
+  const filterAllBookingList = (filteredData: BookingType[]) => {
+    return filteredData.filter((book) => {
+      return (
+        pattern.test(String(book.room_avaliable_id)) ||
+        pattern.test(book.room_details.room_type) ||
+        pattern.test(book.room_status)
+      );
+    });
+  };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSelectedByText(event.target.value);
+  };
+
+  useEffect(() => {
+    getBooking();
+  }, []);
+
+  useEffect(() => {
+    if (selectedByText) {
+      const combinedData = filterAllBookingList(booking);
+
+      const timer = setTimeout(() => {
+        setFilterBookingList(combinedData);
+      }, 400);
+
+      return () => clearTimeout(timer);
+    }
+    if (!selectedByText) {
+      setFilterBookingList(booking);
+    }
+  }, [selectedByText]);
+
+  let isCancelled;
+
+  const rows = filterBookingList.map((book) => {
+    isCancelled = book.status === "cancel";
+
+    return createData(
+      book.room_avaliable_id,
+      book.room_details.room_type,
+      book.room_details.bed_types,
+      book.room_status
+      // isCancelled
+    );
+  });
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const StyledTableCell = styled(TableCell)(() => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: "#E4E6ED",
+      fontSize: 14,
+      fontWeight: 500,
+      fontFamily: "Inter",
+      color: "#424C6B",
+      padding: "10px 16px",
+    },
+    [`&.${tableCellClasses.body}`]: {
+      backgroundColor: "white",
+      fontSize: 16,
+      fontFamily: "Inter",
+      color: "black",
+      borderColor: "#E4E6ED",
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(() => ({
+    backgroundColor: "#000000",
+    // hide last border
+    "&:last-child td, &:last-child th": {
+      border: 0,
+    },
+  }));
+
+  function createData(
+    roomNumber: number,
+    roomType: string,
+    bedType: string,
+    roomStatus: string
+  ) {
+    return { roomNumber, roomType, bedType, roomStatus };
+  }
+  const roomNumberArr = rows.map((row) => row.roomNumber);
+  roomNumberArr.sort((a, b) => a - b);
+  // console.log(roomNumberArr);
+
   return (
     <>
-      <div className=" w-full">
-         {/* navbar field*/}
+      <div className="bg-gray-100 h-screen">
+        {/* navbar field*/}
         <div className="bg-white h-20 min-w-[1295px] w-full flex flex-row items-center drop-shadow-md">
           <div className="flex flex-row w-full justify-between items-center pl-16 pr-7">
-            <p className=" text-black font-bold">Room Management</p>
+            <p className="text-black font-bold">Room Management</p>
             <div>
               <FormControl>
                 <OutlinedInput
+                  value={selectedByText}
                   placeholder="Search…"
+                  onChange={handleInputChange}
                   size="small"
                   id="input-with-icon-adornment"
                   inputProps={{
@@ -89,42 +266,93 @@ function RoomManagement() {
             </div>
           </div>
         </div>
+
         {/* table field*/}
-        <div className="bg-gray-100 w-full py-16 px-12">
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell>Customer</StyledTableCell>
-                  <StyledTableCell>Guest(s)</StyledTableCell>
-                  <StyledTableCell>Room type</StyledTableCell>
-                  <StyledTableCell>Amount</StyledTableCell>
-                  <StyledTableCell>Bed type</StyledTableCell>
-                  <StyledTableCell>Check-in</StyledTableCell>
-                  <StyledTableCell>Check-out</StyledTableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow>
-                    <StyledTableCell component="th" scope="row">
-                      {row.customerName}
+        <div className="px-24 py-12">
+          <Paper
+            sx={{
+              overflow: "hidden",
+            }}
+          >
+            <TableContainer component={Paper}>
+              <Table aria-label="custom pagination table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell className="w-[20%]">
+                      Room no.
                     </StyledTableCell>
-                    <StyledTableCell>{row.guest}</StyledTableCell>
-                    <StyledTableCell>{row.roomType}</StyledTableCell>
-                    <StyledTableCell>{row.amount}</StyledTableCell>
-                    <StyledTableCell>{row.bedType}</StyledTableCell>
-                    <StyledTableCell>{row.checkIn}</StyledTableCell>
-                    <StyledTableCell>{row.checkOut}</StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+                    <StyledTableCell className="w-[20%]">
+                      Room type
+                    </StyledTableCell>
+                    <StyledTableCell className="w-[35%]">
+                      Bed type
+                    </StyledTableCell>
+                    <StyledTableCell className="w-[35%]">
+                      Status
+                    </StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {(rowsPerPage > 0
+                    ? roomNumberArr.slice(
+                        page * rowsPerPage,
+                        page * rowsPerPage + rowsPerPage
+                      )
+                    : roomNumberArr
+                  ).map((roomNumber, index) => {
+                    const row = rows.find(
+                      (row) => row.roomNumber === roomNumber
+                    );
+
+                    return row ? (
+                      <StyledTableRow key={index}>
+                        <StyledTableCell>
+                          {roomNumber > 9
+                            ? `0${roomNumber}`
+                            : `00${roomNumber}`}
+                        </StyledTableCell>
+                        <StyledTableCell>{row.roomType}</StyledTableCell>
+                        <StyledTableCell>{row.bedType}</StyledTableCell>
+                        <StyledTableCell>
+                          <DropdownSearch
+                            roomNumber={roomNumber}
+                            roomStatus={row.roomStatus}
+                          />
+                        </StyledTableCell>
+                      </StyledTableRow>
+                    ) : null;
+                  })}
+                  {emptyRows > 0 && (
+                    <TableRow style={{ height: 53 * emptyRows }}>
+                      <TableCell colSpan={6} />
+                    </TableRow>
+                  )}
+                </TableBody>
+                <TableFooter>
+                  <TableRow>
+                    <TablePagination
+                      rowsPerPageOptions={[10]}
+                      colSpan={3}
+                      count={rows.length}
+                      rowsPerPage={rowsPerPage}
+                      page={page}
+                      SelectProps={{
+                        inputProps: {
+                          "aria-label": "rows per page",
+                        },
+                        native: true,
+                      }}
+                      onPageChange={handleChangePage}
+                      onRowsPerPageChange={handleChangeRowsPerPage}
+                      ActionsComponent={TablePaginationActions}
+                    />
+                  </TableRow>
+                </TableFooter>
+              </Table>
+            </TableContainer>
+          </Paper>
         </div>
       </div>
     </>
   );
 }
-
-export default RoomManagement;
