ad3ceebd378349023bba347e3bbdd9032697180e
Tongcnc <tongcnc3@gmail.com>
2023-10-03 19:19:58 +0700

    feat: add Loading to BookHist

--- src/contexts/BookingContext.tsx
+++ src/contexts/BookingContext.tsx
@@ -1,13 +1,14 @@
 import { createContext, useState, useEffect } from "react";
 import axios from "axios";
 import { useAuth } from "./authen";
+import Loader from "../components/Loader";
 
 const BookingsContext = createContext();
 
 export function BookingsProvider({ children }: any) {
   const [bookingsHistory, setBookingsHistory] = useState([]);
   const auth = useAuth();
-
+  const [isLoading, setIsLoading] = useState(true);
   // console.log(bookingsHistory);
 
   const getBookingsHistory = async () => {
@@ -25,12 +26,14 @@ export function BookingsProvider({ children }: any) {
   };
 
   useEffect(() => {
-    getBookingsHistory();
+    getBookingsHistory().then(() => {
+      setIsLoading(false);
+    });
   }, [auth.state.userData]);
 
   return (
     <BookingsContext.Provider value={{ bookingsHistory }}>
-      {children}
+      {isLoading ? <Loader /> : <>{children}</>}
     </BookingsContext.Provider>
   );
 }
